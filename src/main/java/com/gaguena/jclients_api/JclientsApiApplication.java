cf599e94233195a63e8a451192c2703edd6d07f7
Gabriel Aguena <gaguena@gmail.com>
2025-11-09 12:50:54 -0300

    first commit

---
+++ src/main/java/com/gaguena/jclients_api/JclientsApiApplication.java
@@ -0,0 +1,13 @@
+package com.gaguena.jclients_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JclientsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JclientsApiApplication.class, args);
+	}
+
+}
